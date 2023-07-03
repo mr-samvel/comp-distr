3ea7cf603bd90cf8a62ea5a05c013b89942abd54
Samuel Moreira Ransolin <samuransolin@gmail.com>
2023-07-03 12:02:02 -0300

    ?

--- final/communication_lib.py
+++ final/communication_lib.py
@@ -1,59 +1,69 @@
-from queue import Queue
-from collections import defaultdict
+import socket
+import json
 
 class CommunicationLib:
-    def __init__(self, num_processes):
-        self.num_processes = num_processes
-        self.message_queue = Queue()
-        self.pending_messages = defaultdict(list)
-        self.process_clock = [0] * num_processes
+    def __init__(self, process_id, processes):
+        self.process_id = process_id
+        self.processes = processes
+        self.timestamps = {p: 0 for p in processes}
+        self.pending_messages = []
 
-    def send(self, recipient_id, msg):
-        timestamp = self.increment_clock(recipient_id)
-        self.pending_messages[recipient_id].append((timestamp, msg))
+    def send(self, message, destination):
+        timestamp = self.increment_timestamp()
+        self.pending_messages.append((timestamp, message, destination))
+        self.broadcast_pending_messages()
 
-    def receive(self, msg):
-        self.message_queue.put(msg)
-        self.process_pending_messages()
+    def receive(self, message):
+        timestamp, sender = message['timestamp'], message['sender']
+        self.timestamps[sender] = max(self.timestamps[sender], timestamp) + 1
+        self.pending_messages.append((timestamp, message, None))
+        self.broadcast_pending_messages()
 
-    def broadcast(self, msg):
-        timestamp = self.increment_clock()
-        self.pending_messages['broadcast'].append((timestamp, msg))
-        self.process_pending_messages()
+    def broadcast(self, message):
+        timestamp = self.increment_timestamp()
+        self.pending_messages.append((timestamp, message, None))
+        self.broadcast_pending_messages()
 
-    def deliver(self, msg):
-        self.message_queue.put(msg)
-        self.process_pending_messages()
+    def deliver(self):
+        self.pending_messages.sort(key=lambda x: x[0])
+        delivered_messages = []
+        for _, message, destination in self.pending_messages:
+            if destination is None or destination == self.process_id:
+                delivered_messages.append(message)
+        self.pending_messages = []
+        return delivered_messages
 
-    def process_pending_messages(self):
-        while not self.message_queue.empty():
-            msg = self.message_queue.get()
+    def increment_timestamp(self):
+        self.timestamps[self.process_id] += 1
+        return self.timestamps[self.process_id]
 
-            if isinstance(msg, tuple):
-                timestamp, content = msg
-                self.process_clock[timestamp[0]] = max(
-                    self.process_clock[timestamp[0]], timestamp[1]
-                )
-            else:
-                content = msg
+    def broadcast_pending_messages(self):
+        for process in self.processes:
+            if process != self.process_id:
+                self.send_pending_messages(process)
 
-            # Process messages intended for a specific recipient
-            if content['type'] == 'unicast':
-                recipient_id = content['recipient_id']
-                if recipient_id != 'broadcast':
-                    if self.can_deliver(content['timestamp']):
-                        self.message_queue.put(content['message'])
-            # Process broadcast messages
-            elif content['type'] == 'broadcast':
-                if self.can_deliver(content['timestamp']):
-                    self.message_queue.put(content['message'])
+    def send_pending_messages(self, destination):
+        with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
+            try:
+                sock.connect(('localhost', destination))
+                for _, message, _ in self.pending_messages:
+                    data = {
+                        'timestamp': self.timestamps[self.process_id],
+                        'sender': self.process_id,
+                        'message': message
+                    }
+                    sock.sendall(json.dumps(data).encode())
+            except ConnectionRefusedError:
+                print(f"Connection refused to process {destination}")
 
-    def can_deliver(self, timestamp):
-        process_id, timestamp_value = timestamp
-        return self.process_clock[process_id] == timestamp_value - 1
-
-    def increment_clock(self, process_id=None):
-        if process_id is None:
-            process_id = 'broadcast'
-        self.process_clock[process_id] += 1
-        return (process_id, self.process_clock[process_id])
+    def start_listening(self):
+        with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
+            sock.bind(('localhost', self.process_id))
+            sock.listen()
+            while True:
+                conn, addr = sock.accept()
+                data = conn.recv(1024).decode()
+                if data:
+                    message = json.loads(data)
+                    self.receive(message)
+                conn.close()
--- final/main.py
+++ final/main.py
@@ -1,18 +1,41 @@
 from communication_lib import CommunicationLib
+import threading
+import time
 
-# Initialize the communication library with the number of processes
-comm = CommunicationLib(num_processes=3)
+# Define the number of processes and their IDs
+NUM_PROCESSES = 3
+PROCESS_IDS = [0, 1, 2]
 
-# Process 0 sends a unicast message to Process 1
-comm.send(1, {'type': 'unicast', 'recipient_id': 1, 'message': 'Hello from Process 0!', 'timestamp': comm.increment_clock(0)})
+# Create an instance of the CausalOrdering class for each process
+processes = []
+for process_id in PROCESS_IDS:
+    processes.append(CommunicationLib(process_id, PROCESS_IDS))
 
-# Process 2 broadcasts a message to all processes
-comm.broadcast({'type': 'broadcast', 'message': 'Broadcast message from Process 2!', 'timestamp': comm.increment_clock(2)})
+# Function to simulate sending messages between processes
+def simulate_send_receive(process_id, destination_id, message):
+    time.sleep(1)  # Simulate some processing time before sending the message
+    processes[process_id].send(message, destination_id)
 
-# Process 1 receives the unicast and broadcast messages
-comm.receive({'type': 'unicast', 'recipient_id': 1, 'message': 'Hello from Process 0!', 'timestamp': comm.increment_clock(0)})
-comm.deliver({'type': 'broadcast', 'message': 'Broadcast message from Process 2!', 'timestamp': comm.increment_clock(2)})
+# Function to simulate receiving messages by a process
+def simulate_receive(process_id):
+    while True:
+        time.sleep(0.5)  # Simulate some processing time before checking for messages
+        delivered_messages = processes[process_id].deliver()
+        for message in delivered_messages:
+            print(f"Process {process_id} received message: {message}")
 
-# Check the message queue of Process 1
-while not comm.message_queue.empty():
-    print(comm.message_queue.get())
+# Start a thread for each process to simulate receiving messages
+receive_threads = []
+for process_id in PROCESS_IDS:
+    thread = threading.Thread(target=simulate_receive, args=(process_id,))
+    receive_threads.append(thread)
+    thread.start()
+
+# Simulate sending messages between processes
+simulate_send_receive(0, 1, "Hello from Process 0 to Process 1!")
+simulate_send_receive(1, 2, "Hello from Process 1 to Process 2!")
+simulate_send_receive(2, 0, "Hello from Process 2 to Process 0!")
+
+# Wait for all receive threads to finish
+for thread in receive_threads:
+    thread.join()
